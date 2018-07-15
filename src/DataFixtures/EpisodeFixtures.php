450f98d3faea744702e2ff8512f4f94cede54c6b
barbaris <barbaris.in@gmail.com>
2018-07-15 23:52:53 +0300

    Update EpisodeFixtures to use Faker

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -10,36 +10,10 @@ class EpisodeFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $episode = new Episode();
-        $episode
-            ->setTitle('Latin episode')
-            ->setBody('Vae, brevis historia! Nunquam dignus domina. Luras sunt liberis de varius adiurator.');
-        $manager->persist($episode);
-
-        $episode = new Episode();
-        $episode
-            ->setTitle('Esoteric episode')
-            ->setBody('Reincarnation is not soft in earth, the monastery, or space, but everywhere. Samadhi, peace and a unveiled heavens of enlightenment.');
-        $manager->persist($episode);
-
-        $episode = new Episode();
-        $episode
-            ->setTitle('Culinary episode')
-            ->setBody('Enamel eleven pounds of bagel in one quarter cup of vinegar. ');
-        $manager->persist($episode);
-
-        $episode = new Episode();
-        $episode
-            ->setTitle('Pirate episode')
-            ->setBody('Ahoy, oh. Aww, ye heavy-hearted wave- set sails for strength! Aww, yer not drinking me without a fortune!');
-        $manager->persist($episode);
-
-        $episode = new Episode();
-        $episode
-            ->setTitle('Science episode')
-            ->setBody('Girls yell with history at the devastated ready room! The sensor is finally chemical.');
-        $manager->persist($episode);
-
-        $manager->flush();
+        $generator = \Faker\Factory::create();
+        $generator->seed(1);
+        $populator = new \Faker\ORM\Doctrine\Populator($generator, $manager);
+        $populator->addEntity(Episode::class, 5);
+        $populator->execute();
     }
 }
